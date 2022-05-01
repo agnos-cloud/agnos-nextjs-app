5b8b2ec3e1033768e6c862148ec8e51b9e18a9b0
chieze-franklin <chieze.franklin@gmail.com>
2022-05-01 23:34:08 +0100

    set up logs table

--- components/FunctionLogs.tsx
+++ components/FunctionLogs.tsx
@@ -1,9 +1,86 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import type { Log } from "../models/Log";
+import { LogType } from "../constants/log";
+
+const columns: GridColDef[] = [
+  // { field: "id", headerName: "ID", width: 300 },
+  {
+    field: "createdAt",
+    headerName: "Date",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) =>
+      new Date(params.value).toDateString(),
+  },
+  {
+    field: "env",
+    headerName: "Env",
+    width: 150,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 150,
+  },
+  {
+    field: "meta",
+    headerName: "Version",
+    width: 150,
+    valueGetter: (params: GridValueGetterParams) => params.value["versionName"],
+  },
+  {
+    field: "data",
+    headerName: "Data",
+    width: 400,
+    valueGetter: (params: GridValueGetterParams) => String(params.value),
+  },
+];
+
 export interface FunctionLogsProps {
   functionId?: string;
 }
 
 function FunctionLogs(props: FunctionLogsProps) {
-  return <>hello world</>;
+  const { functionId } = props;
+  const [logs, setLogs] = useState<Log[]>([]);
+  const pageSize = 10;
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          "& .grid-row--even": {
+            bgcolor: "aliceblue",
+          },
+          "& .grid-row--error": {
+            bgcolor: "red",
+          },
+        }}
+      >
+        <DataGrid
+          rows={logs.map((v) => ({ ...v, id: v._id }))}
+          columns={columns}
+          pageSize={pageSize}
+          rowsPerPageOptions={[pageSize]}
+          getRowClassName={(params) =>
+            `grid-row--${
+              params.row["type"] === LogType.ERROR
+                ? "error"
+                : params.indexRelativeToCurrentPage % 2 === 0
+                ? "even"
+                : "odd"
+            }`
+          }
+          // onRowClick={handleRowClick}
+          autoHeight
+          // checkboxSelection
+          disableSelectionOnClick
+        />
+      </Box>
+    </Box>
+  );
 }
 
 export default FunctionLogs;
---
+++ models/Log.ts
@@ -0,0 +1,12 @@
+import type { Env } from "../constants/env";
+import type { DataType, LogType } from "../constants/log";
+import type { Model } from "./Model";
+
+export interface Log extends Model {
+  data: any;
+  dataType?: DataType;
+  env: Env;
+  meta?: any;
+  source: string;
+  type?: LogType;
+}
